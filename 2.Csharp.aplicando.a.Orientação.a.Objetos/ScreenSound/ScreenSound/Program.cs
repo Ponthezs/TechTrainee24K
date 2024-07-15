7300350981392bb4d17ed12d75cd34b634a84137
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-07-15 19:33:33 -0300

    feat

---
+++ 2.Csharp.aplicando.a.Orientação.a.Objetos/ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,18 @@
+﻿Musica musica1 = new Musica();
+ 
+musica1.Nome = "3 Dias Virados";
+musica1.Artista = "IG";
+musica1.Duracao = 312;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
+
+
+Musica musica2 = new Musica();
+musica2.Nome = "Roxane";
+musica2.Artista = "The Police";
+musica2.Duracao = 215;
+musica2.Disponivel = false;
+Console.WriteLine(musica2.Disponivel);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
