bf925ecf28cfb78006e2452e8919a11e69d6d6a0
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-07-25 19:54:19 -0300

    feat

--- 4 - Csharp consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04/ScreenSound-04/Program.cs
+++ 4 - Csharp consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04/ScreenSound-04/Program.cs
@@ -12,7 +12,31 @@ using (HttpClient client = new HttpClient())
         //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
         // LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
         // LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
-        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michel Teló");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michel Teló");
+
+
+        //var musicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
+
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[377]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[4]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[6]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1467]);
+
+        //musicasPreferidasDoDaniel.ExibirMusicasFavoritas();
+
+        var musicasPreferidasEmilly = new MusicasPreferidas("Emy");
+
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[500]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[637]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[428]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[13]);
+        musicasPreferidasEmilly.AdicionarMusicasFavoritas(musicas[71]);
+
+        musicasPreferidasEmilly.ExibirMusicasFavoritas();
+
+        musicasPreferidasEmilly.GerarArquivoJson();
+
     }
     catch (Exception ex)
     {
