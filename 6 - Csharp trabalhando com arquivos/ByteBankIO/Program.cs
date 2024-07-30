e84ebeb6e4d2f3e89df35d46de3734d31565ccfe
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-07-30 18:35:52 -0300

    feat

---
+++ 6 - Csharp trabalhando com arquivos/ByteBankIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using ByteBankIO;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        EscritaBinaria();
+        LeituraBinaria();
+        Console.WriteLine("Aplicação Finalizada ...");
+        Console.ReadLine();
+    }
+}
