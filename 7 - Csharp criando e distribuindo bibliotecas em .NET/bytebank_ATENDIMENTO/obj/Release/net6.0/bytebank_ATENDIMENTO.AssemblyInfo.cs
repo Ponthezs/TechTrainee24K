9883da11ebeadfc925f28ac4bcb3d5f8a37f3a71
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-08-01 20:18:36 -0300

    feat

--- 7 - Csharp criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/obj/Release/net6.0/bytebank_ATENDIMENTO.AssemblyInfo.cs
+++ 7 - Csharp criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/obj/Release/net6.0/bytebank_ATENDIMENTO.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("bytebank_Modelos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("bytebank_ATENDIMENTO")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce8743661a9f24e7346a260c6afde965b38a06a")]
-[assembly: System.Reflection.AssemblyProductAttribute("bytebank_Modelos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("bytebank_Modelos")]
+[assembly: System.Reflection.AssemblyProductAttribute("bytebank_ATENDIMENTO")]
+[assembly: System.Reflection.AssemblyTitleAttribute("bytebank_ATENDIMENTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- 7 - Csharp criando e distribuindo bibliotecas em .NET/bytebank_Modelos/obj/Release/net6.0/bytebank_Modelos.AssemblyInfo.cs
+++ 7 - Csharp criando e distribuindo bibliotecas em .NET/bytebank_Modelos/obj/Release/net6.0/bytebank_Modelos.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("bytebank_Modelos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce8743661a9f24e7346a260c6afde965b38a06a")]
 [assembly: System.Reflection.AssemblyProductAttribute("bytebank_Modelos")]
