badac6fc4c6b8ca18be9b2ebc994edd2b81ba6c3
Felipe Pontes <pontesfelipe835@gmail.com>
2024-08-01 22:56:32 -0300

    feat

--- 7 - Csharp criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/obj/Release/net6.0/bytebank_ATENDIMENTO.AssemblyInfo.cs
+++ 7 - Csharp criando e distribuindo bibliotecas em .NET/bytebank_ATENDIMENTO/obj/Release/net6.0/bytebank_ATENDIMENTO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bytebank_ATENDIMENTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce8743661a9f24e7346a260c6afde965b38a06a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9883da11ebeadfc925f28ac4bcb3d5f8a37f3a71")]
 [assembly: System.Reflection.AssemblyProductAttribute("bytebank_ATENDIMENTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bytebank_ATENDIMENTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
