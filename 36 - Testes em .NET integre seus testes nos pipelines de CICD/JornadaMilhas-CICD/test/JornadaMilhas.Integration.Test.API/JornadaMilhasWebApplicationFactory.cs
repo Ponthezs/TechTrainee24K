8d6b7fe3146ece10116223483d761d32abd703e7
Felipe Pontes <pontesfelipe835@gmail.com>
2025-01-14 17:00:47 -0300

    feat

---
+++ 36 - Testes em .NET integre seus testes nos pipelines de CICD/JornadaMilhas-CICD/test/JornadaMilhas.Integration.Test.API/JornadaMilhasWebApplicationFactory.cs
@@ -0,0 +1,69 @@
+﻿using JornadaMilhas.API.DTO.Auth;
+using JornadaMilhas.Dados;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Testcontainers.MsSql;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class JornadaMilhasWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        public JornadaMilhasContext Context { get; private set; }
+
+        private IServiceScope scope;
+
+        private readonly MsSqlContainer _mssqlContainer = new MsSqlBuilder()
+     .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+     .Build();
+
+             
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<JornadaMilhasContext>));
+                services.AddDbContext<JornadaMilhasContext>(options =>
+           options
+           .UseLazyLoadingProxies()
+           .UseSqlServer(_mssqlContainer.GetConnectionString()));
+            });
+
+            base.ConfigureWebHost(builder);
+        }
+
+        public async Task<HttpClient> GetClientWithAccessTokenAsync()
+        {
+            var client = this.CreateClient();
+            var user = new UserDTO { Email = "tester@email.com", Password = "Senha123@" };
+            var resultado = await client.PostAsJsonAsync("/auth-login", user);
+
+            resultado.EnsureSuccessStatusCode();
+
+            var result = await resultado.Content.ReadFromJsonAsync<UserTokenDTO>();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result!.Token);
+
+            return client;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _mssqlContainer.StartAsync();
+            this.scope = Services.CreateScope();
+            Context = scope.ServiceProvider.GetRequiredService<JornadaMilhasContext>();
+
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _mssqlContainer.DisposeAsync();
+        }
+
+    }
+}
